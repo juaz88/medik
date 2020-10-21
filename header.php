7e8b240d4c0c8b1d761fab848b68d6b756ba88d1
josee2205 <72671819+josee2205@users.noreply.github.com>
2020-10-21 10:33:09 -0500

    Add files via upload

---
+++ header.php
@@ -0,0 +1,125 @@
+<header class="main-header">
+
+    <head>
+        <meta charset="utf-8">
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+        <title>Rapid Bootstrap Template - Index</title>
+        <meta content="" name="descriptison">
+        <meta content="" name="keywords">
+
+        <!-- Favicons -->
+        <link href="assets/img/favicon.png" rel="icon">
+        <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+        <!-- Google Fonts -->
+        <link
+            href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Montserrat:300,400,500,600,700"
+            rel="stylesheet">
+
+        <!-- Vendor CSS Files -->
+        <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link href="assets/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+        <link href="assets/vendor/ionicons/css/ionicons.min.css" rel="stylesheet">
+        <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+        <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+        <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+
+        <!-- Template Main CSS File -->
+        <link href="assets/css/style.css" rel="stylesheet">
+
+        <!-- =======================================================
+  * Template Name: Rapid - v2.2.0
+  * Template URL: https://bootstrapmade.com/rapid-multipurpose-bootstrap-business-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+    </head>
+
+    <body>
+
+        <!-- ======= Top Bar ======= -->
+        <div id="topbar" class="d-none d-lg-flex align-items-end fixed-top topbar-transparent">
+            <div class="container d-flex justify-content-end">
+                <div class="social-links">
+                    <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+                    <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
+                    <a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>
+                    <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
+                </div>
+            </div>
+        </div>
+
+        <!-- ======= Header ======= -->
+        <header id="header" class="fixed-top header-transparent">
+            <div class="container d-flex align-items-center">
+
+                <h1 class="logo mr-auto"><a href="index.html">MediK</a></h1>
+                <!-- Uncomment below if you prefer to use an image logo -->
+                <!-- <a href="index.html" class="logo mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+                <nav class="main-nav d-none d-lg-block">
+                    <ul>
+                        <li><a href="#inicio"><a href="index.html">Inicio</a></li>
+                        <li><a href="#about"><a href="about.html">Sobre nosotros</a></li>
+                        <li><a href="#services"><a href="services.html">Servicios</a></li>
+                        <li><a href="#contact"><a href="plantilla.html">Contacto</a></li>
+                        <li><a href="#login"><a href="login.php">Login</a></li>
+                    </ul>
+                </nav>
+                <!-- .main-nav-->
+
+            </div>
+        </header>
+
+
+        <main id="main">
+
+            <!-- ======= About Section ======= -->
+
+
+            <!-- ======= Services Section ======= -->
+
+            <!-- End Services Section -->
+
+            <!-- ======= Why Us Section ======= -->
+
+
+            <!-- ======= Call To Action Section ======= -->
+
+
+            <!-- ======= Portfolio Section ======= -->
+
+            <!-- ======= Team Section ======= -->
+
+
+            <!-- ======= Clients Section ======= -->
+
+
+            <!-- ======= F.A.Q Section ======= -->
+
+
+
+
+            <!-- ======= Footer ======= -->
+
+
+            <a href="# " class="back-to-top "><i class="fa fa-chevron-up "></i></a>
+
+            <!-- Vendor JS Files -->
+            <script src="assets/vendor/jquery/jquery.min.js "></script>
+            <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js "></script>
+            <script src="assets/vendor/jquery.easing/jquery.easing.min.js "></script>
+            <script src="assets/vendor/php-email-form/validate.js "></script>
+            <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js "></script>
+            <script src="assets/vendor/counterup/counterup.min.js "></script>
+            <script src="assets/vendor/venobox/venobox.min.js "></script>
+            <script src="assets/vendor/owl.carousel/owl.carousel.min.js "></script>
+            <script src="assets/vendor/waypoints/jquery.waypoints.min.js "></script>
+            <script src="assets/vendor/aos/aos.js "></script>
+
+            <!-- Template Main JS File -->
+            <script src="assets/js/main.js "></script>
+        </main>
+    </body>
+</header>
