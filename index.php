abc275b20b5f95e1d119ad0887a792e894602389
josee2205 <72671819+josee2205@users.noreply.github.com>
2020-10-24 11:53:52 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,89 @@
+<?php include_once('header.php'); ?>
+  <!DOCTYPE html>
+<html lang="es">
+
+    <!-- ======= Hero Section ======= -->
+    <section id="hero" class="clearfix">
+        <div class="container d-flex h-100">
+            <div class="row justify-content-center align-self-center" data-aos="fade-up">
+                <div class="col-md-6 intro-info order-md-first order-last" data-aos="zoom-in" data-aos-delay="100">
+                    <h2>Portege tu salud<br>Pide desde casa con <span>Seguridad plena</span></h2>
+                    <div>
+                        <a href="login.php" class="btn-get-started scrollto">Iniciar sesion</a>
+                    </div>
+                </div>
+
+                <div class="col-md-6 intro-img order-md-last order-first" data-aos="zoom-out" data-aos-delay="200">
+                    <img src="assets/img/n_intro-img.png" alt="" class="img-fluid">
+                </div>
+            </div>
+
+        </div>
+    </section>
+    <!-- End Hero -->
+
+    <main id="main">
+
+        <!-- ======= About Section ======= -->
+
+
+        <!-- ======= Services Section ======= -->
+        <section id="services" class="services section-bg">
+            <div class="container" data-aos="fade-up">
+
+                <header class="section-header">
+                    <h3>Servicios</h3>
+                    <p>Con esta aplicacion puedes realizar pedidos directamente a tu farmacia favorita</p>
+                </header>
+
+                <div class="row">
+
+                    <div class="col-md-6 col-lg-4 wow bounceInUp" data-aos="zoom-in" data-aos-delay="100">
+                        <div class="box">
+                            <div class="icon" style="background: #fceef3;"><i class="ion-ios-analytics-outline" style="color: #ff689b;"></i></div>
+                            <h4 class="title"><a href="">Realiza un pedido</a></h4>
+                            <p class="description">Puedes realizar un pedido seleccionando los productos o escaneando tu formula medica!</p>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-4" data-aos="zoom-in" data-aos-delay="200">
+                        <div class="box">
+                            <div class="icon" style="background: #fff0da;"><i class="ion-ios-bookmarks-outline" style="color: #e98e06;"></i></div>
+                            <h4 class="title"><a href="">Ver productos</a></h4>
+                            <p class="description">Puedes ver todos los productos disponibles en tu farmacia favorita!</p>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 col-lg-4" data-aos="zoom-in" data-aos-delay="200">
+                        <div class="box">
+                            <div class="icon" style="background: #fff0da;"><i class="ion-ios-bookmarks-outline" style="color: #e98e06;"></i></div>
+                            <h4 class="title"><a href="">Farmacias</a></h4>
+                            <p class="description">Puedes registrar tu farmacia y disfrutar de todos nuestros beneficios!!</p>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 col-lg-4" data-aos="zoom-in" data-aos-delay="200">
+                    </div>
+
+                </div>
+        </section>
+        <!-- End Services Section -->
+
+        <!-- ======= Why Us Section ======= -->
+
+
+        <!-- ======= Call To Action Section ======= -->
+
+
+        <!-- ======= Portfolio Section ======= -->
+
+        <!-- ======= Team Section ======= -->
+
+
+        <!-- ======= Clients Section ======= -->
+
+
+        <!-- ======= F.A.Q Section ======= -->
+</body>
+
+</html>
+<?php include_once('footer.php'); ?>
