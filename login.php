115dd7704d2c92d1390ec459c0257675855f5420
josee2205 <72671819+josee2205@users.noreply.github.com>
2020-10-24 12:16:19 -0500

    Add files via upload

---
+++ login.php
@@ -0,0 +1,6 @@
+<?php
+
+include("loginconnection.php");
+include("loginhtml.php");
+
+?>
---
+++ loginhtml.php
@@ -0,0 +1,69 @@
+<?php include_once('header.php'); ?>
+<html>
+<style>
+
+table
+{
+border: 2px solid #6196ed;
+background-color: #dae3f2;
+}
+
+input[type=text], input[type=password]
+{
+width: 100%;
+padding: 8px 20px;
+border: 2px solid #6196ed;
+box-sizing: border-box;
+}
+body
+{
+background-color: #f5f8fd;
+}
+
+img
+{
+width: 100%;
+height: 100%;
+}
+
+label
+{
+font-size: 14px;
+font-weight: bold;
+font-family: arial;
+}
+
+input[type=submit]
+{
+background-color: #000080;
+color: white;
+padding: 8px 10px;
+margin: 8px 0px
+border: solid;
+cursor: pointer;
+width: 40%;
+}
+</style>
+
+<center>
+<form method="post" action="login.php">
+<br><br><br><br><br><br><br><br>
+<table>
+
+<tr><td colspan="2" style="background-color:#c5d4ed; padding-bottom:5px; padding-top:5px;"><label>Login</label></td></tr>
+<tr><td align="center" rowspan="5"><img src="uploads/logo.png"/></td><td><label>Usuario</label></td></tr>
+<br>
+<tr><td><input type="text" name="txtusuario"/></td></tr>
+<br>
+<tr><td><label>Password</label></td></tr>
+<tr><td><input type="password" name="txtpassword" /> </td></tr>
+<tr><td><input type="submit" value="ingresar" /> </td></tr>
+<tr><td> <a href="registrarse.php" target="_blank">Registrarse</a><p><a href="recuperarpassword.php" target="_blank">Olvide Contraseña</a></td></tr>
+</table>
+<br>
+<br>
+<br>
+</form>
+</center>
+</html>
+<?php include_once('footer.php'); ?>
