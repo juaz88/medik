e8c7177f66f094f60a5082d0474e609f0ceb3c5c
josee2205 <72671819+josee2205@users.noreply.github.com>
2020-10-24 12:10:32 -0500

    Add files via upload

---
+++ loginconnection.php
@@ -0,0 +1,29 @@
+<?php
+
+$dbhost = "127.0.0.1";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "medik";
+
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+if (!$conn)
+{
+	die("No hay conexión: ".mysqli_connect_error());
+}
+
+$nombre = $_POST["txtusuario"];
+$pass = $_POST["txtpassword"];
+
+$query = mysqli_query ($conn, "SELECT * FROM login WHERE usuario = '".$nombre."' and password = '".$pass."'");
+$nr = mysqli_num_rows ($query);
+
+if ($nr == 1)
+{
+	header("Location: principal.php");
+}
+else if ($nr == 0)
+{
+	header("Location: login.html");
+}
+
+?>
